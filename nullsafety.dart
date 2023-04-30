be4b33bb05f13cf245aed35be57eb06380dd7727
Anjila21 <anjila.tripathi@gmail.com>
2023-04-30 18:21:42 +0545

    lab work

---
+++ nullsafety.dart
@@ -0,0 +1,13 @@
+//Null Safety in Dart
+void main() {
+  String? name;
+  name = "John";
+  name = null;
+  if (name == null) {
+    print("Name is null");
+  }
+  String name1 = name ?? "Stranger";
+  print(name1);
+  String name2 = name!;
+  print(name2);
+}
